ffd5c7351e2eb382307ec7ab264962c7727caf84
EladioNavarro <eladio.k.7@gmail.com>
2025-05-16 10:39:31 +0200

    ultimo cambio

---
+++ src/tests/testTrianguloRectangulo.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import app.Principal;
+
+class testTrianguloRectangulo {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	private int a;
+	private int b;
+	private double c;
+	
+	
+	public double area() {
+		double area = ((a*b)/2);
+		return area;
+		
+	}
+	
+	public double hipotenusa() {
+		double c = Math.sqrt(a^2+b^2);
+		return c;
+	}
+	
+	public double perimetro() {
+		double perimetroCalculado = a+b+c;
+		return perimetroCalculado;
+	}
+
+}
