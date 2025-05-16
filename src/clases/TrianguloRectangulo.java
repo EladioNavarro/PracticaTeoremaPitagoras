f1b22767166b5ea8024539cfd7bc15501900ff2c
EladioNavarro <eladio.k.7@gmail.com>
2025-05-16 10:01:23 +0200

    acabo de crear algunos metodos

--- src/clases/TrianguloRectangulo.java
+++ src/clases/TrianguloRectangulo.java
@@ -1,5 +1,58 @@
 package clases;
 
 public class TrianguloRectangulo {
+	
+	private int a;
+	private int b;
+	private double c;
+	
+	//Metodo constructor
+	
+	public TrianguloRectangulo(int a, int b) {
+		
+		this.a= a;
+		this.b=b;
+	}
+	
+	public int triangulo(int a, int b) {
+		
+		if (a<=0 || b<=0) {
+			//Aqui tiene que lanzar el IllegalArgumentException
+		}
+		return a;
+		
+	}
+	
+	public double area() {
+		double area = ((a*b)/2);
+		return area;
+		
+	}
+	
+	public double hipotenusa() {
+		double c = Math.sqrt(a^2+b^2);
+		return c;
+	}
+	
+	public double perimetro() {
+		double perimetroCalculado = a+b+c;
+		return perimetroCalculado;
+	}
+	
+	public void setA(int a) {
+		this.a=a;
+		System.out.println(a);
+	}
+	
+	public void setB(int b) {
+		this.b=b;
+		System.out.println(b);
+	}
+	
+	public String getValores() {
+		System.out.println(this.a)
+		System.out.println(this.b)
+		return this.a;
+	}
 
 }
